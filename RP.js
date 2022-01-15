5ff9fb79fc40cc07c2ff3773f2e7d9e9d115e03f
Rafael <rafaelgreif@freenet.de>
2022-01-15 19:01:21 +0100

    Add files via upload

---
+++ RP.js
@@ -0,0 +1,10 @@
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "conversation";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageText = "Numbani";
+    Discord_UpdatePresence(&discordPresence);
+}
